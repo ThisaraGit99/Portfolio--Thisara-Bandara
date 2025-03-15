2b94dbbb474977b2a0b3edd79106efd3d1d08def
Your NameThisara <thisranavodbandara@gmail.com>
2025-03-15 16:30:55 +0530

    Updated project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Portfolio---Thisara-Bandara/',
+  base: '/Portfolio---Thisara-Bandara',
   server: {
     host: "::",
     port: 8080,
